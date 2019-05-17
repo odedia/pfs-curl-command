e23fb77ae29cc8bd9936f9acb1858e16fb1f349b
odedia <odedia@gmail.com>
2019-05-17 23:16:26 +0300

    Update run-pipeline.sh

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl my-first-channel-channel.default.svc.cluster.local -w '\n' -d PFS
+curl my-first-channel-channel.default.svc.cluster.local -H 'knative-blocking-request:true' -w '\n' -d PFS
